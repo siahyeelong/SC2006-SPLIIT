cc2ce053becd6c6640693ff655e55a950b998071
Paralyseds <ryanteoliangwei@gmail.com>
2025-03-22 15:33:50 +0800

    Done with login

--- SPLIIT/client/src/components/pages/CreateTrip/index.jsx
+++ SPLIIT/client/src/components/pages/CreateTrip/index.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import { Box } from '@mui/material';
-import Header from '../MainUI/Header';
-import TripCreationForm from './TripCreationForm';
+import React from "react";
+import { Box } from "@mui/material";
+import Header from "../MainUI/Header";
+import TripCreationForm from "./TripCreationForm";
 
 function CreateTrip() {
-    return (
-        <Box m='20px'>
-            <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
-                <Header title={'Create Trip'} subtitle={'Create / Join a trip!'} />
-            </Box>
-            <TripCreationForm />
-        </Box>
-    )
+  return (
+    <Box m="20px">
+      <Box
+        display={"flex"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+      >
+        <Header title={"Create Trip"} subtitle={"Create / Join a trip!"} />
+      </Box>
+      <TripCreationForm />
+    </Box>
+  );
 }
 
-export default CreateTrip
+export default CreateTrip;
